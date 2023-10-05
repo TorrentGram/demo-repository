27ccf0c7a4301bf4a9c3e1a0ba62fc75005ca8c0
sangeeth <sangeeth@rivervalleytechnologies.in>
2023-10-05 19:49:48 +0530

    added button to display a text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,23 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String displayText = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'My Widget',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+      appBar: appBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  // Update the text when the button is pressed
+                  displayText = 'Hello World!';
+                });
+              },
+              child: const Text('Fetch'),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            displayText,
+            style: TextStyle(fontSize: 24),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'My Widget',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: const Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)),
+        child: SvgPicture.asset(
+          'assets/icons/back-button.svg',
+          height: 20,
+          width: 20,
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () => {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 20,
+              width: 20,
+            ),
           ),
         ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-      ),
+      ],
     );
   }
 }
